746a7c9708e6d7967801629000529665af9b9d10
yum <antriksh.timepass@gmail.com>
2021-06-09 01:35:09 +0530

    Add SIGINT handler

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "os/exec"
+    "os"
+    "os/signal"
     "flag"
     "strings"
     "fmt"
@@ -15,10 +17,18 @@ func main() {
     cmd = strings.Trim(cmd, "\"")
     cmds := strings.Split(cmd, " ")
     wg.Add(1)
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt)
+    go func(){
+    	for range c {
+	    fmt.Println("EVENT: Process interrupted")    
+	}
+    }()
     go func() {
 	defer wg.Done()
 	c := exec.Command(cmds[0], cmds[1:]...)
         c.Start()
+	fmt.Println("EVENT: Process started")
 	c.Wait()
 	fmt.Println("EVENT: Process is finished")
     }()
